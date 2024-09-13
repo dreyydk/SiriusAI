92e1620a7516dde90413796f08595203bc15712f
Andrei Bacin <andreib.pf@gmail.com>
2024-09-13 13:35:51 -0300

    feat: Add CallToAction section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,36 @@
+import { Button } from "@/components/Button";
+import starsBg from "@/assets/stars.png";
+import gridLines from "@/assets/grid-lines.png";
+
 export const CallToAction = () => {
-  return <section>CallToAction</section>;
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <div
+          className="relative overflow-hidden rounded-xl border border-white/15 py-24"
+          style={{
+            backgroundImage: `url(${starsBg.src})`,
+          }}
+        >
+          <div
+            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            style={{
+              backgroundImage: `url(${gridLines.src})`,
+            }}
+          ></div>
+          <div className="relative">
+            <h2 className="mx-auto max-w-sm text-center text-5xl font-medium tracking-tighter md:text-6xl">
+              AI-driven SEO for everyone
+            </h2>
+            <p className="mx-auto mt-5 max-w-xs px-4 text-center text-lg tracking-tight text-white/70 md:text-xl">
+              Achieve clear and impactful results without any complexity.
+            </p>
+            <div className="mt-8 flex justify-center">
+              <Button>Join Waitlist</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
